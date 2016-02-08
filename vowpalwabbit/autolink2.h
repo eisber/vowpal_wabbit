6657efc54bf606198487bf39f116a77824245c1e
Markus Cozowicz <eisber@eisber.net>
2016-02-08 10:36:28 -0500

    updated macro

--- vowpalwabbit/autolink2.h
+++ vowpalwabbit/autolink2.h
@@ -1,13 +1,25 @@
 #include "reductions.h"
 
 // this will be defined in base_reduction.h
+// if implementor wants to get separate learn/predict methods
+#define SETUP_VTABLE(TREDUCTION) \
+  virtual void GetVTable(LearnMethod<TREDUCTION::Label>& learnMethod, PredictMethod<TREDUCTION::Label>& predictMethod) \
+    { learnMethod = (LearnMethod<Label>)&TREDUCTION::learn; \
+    predictMethod = (LearnMethod<Label>)&TREDUCTION::predict; \
+    }
+
+// if implementor wants to get single predict_or_learn method
 #define SETUP_VTABLE_PREDICTORLEARN(TREDUCTION) \
   virtual void learn(example& ec, TREDUCTION::Label& label) \
   { predict_or_learn<true>(ec, label);} \
+  \
   virtual void predict(example& ec, TREDUCTION::Label& label) \
   { predict_or_learn<false>(ec, label); } \
+  \
   template <bool is_learn> \
-  void predict_or_learn(TREDUCTION::Label& label);
+  void predict_or_learn(TREDUCTION::Label& label); \
+  \
+  SETUP_VTABLE(TREDUCTION)
 
 class AutoLink : public BaseReduction<Label, Label>
 {
